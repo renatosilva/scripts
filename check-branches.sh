173948d3b688b1c3bde6ec50ef878cd03c7c2db8
Renato Silva <br.renatosilva@gmail.com>
2012-10-21 18:15:56 -0200

    Search for branches recursively, allow specifying a different root, help and copyright.

--- check-branches.sh
+++ check-branches.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-cmds="$@"
-[ -z "$cmds" ] && cmds="diff status missing"
+# Check branches 2012.10.21
+# Copyright (c) 2012 Renato Silva
+# GNU GPLv2 licensed
 
-for cmd in $cmds; do
-    for branch in *; do
-        [ -d "$branch" ] || continue
+# This program goes recursively through a directory hierarchy
+# and run specified commands on every Bazaar branch that is found.
+
+check() {
+    branch=$(echo "$0" | sed s/".bzr"//)
+    for cmd in $(echo "$cmds" | sed -E s/", and ([^,])"/" \\1"/ | sed s/","//g); do
         cd "$branch"
         bzr $cmd | grep -v "parent"
         cd ..
     done
-done
+}
 
+root="$1"
+cmds="${@:2}"
+def_cmds="diff, status, and missing"
+[ -z "$1" ] && root="."
+[ -z "$cmds" ] && cmds="$def_cmds"
+export -f check
+export cmds
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [root [commmand]...]"
+    echo "Without arguments, root is current directory, and commands are $def_cmds."
+    exit
+fi
+
+find "$root" -name ".bzr" -type d -exec bash -c check '{}' \;
 echo "Terminado."
 sleep 3
