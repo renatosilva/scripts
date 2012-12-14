9307a62891af018f6be8f6dc70435dada3986d83
Renato Silva <br.renatosilva@gmail.com>
2012-12-13 23:44:08 -0200

    Add some application settings to the backup.

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Backup 2012.12.12
+# Backup 2012.12.13
 # Copyright (c) 2012 Renato Silva
 # GNU GPLv2 licensed
 
@@ -11,6 +11,7 @@ name="Documentos e programas"
 [[ -z "$target" ]] && target="/dados/backup"
 [[ -e "$target" ]] || { echo "Target device not found."; sleep 5; exit 1; }
 
+# Sticky notes and favorites
 temp="$TEMP/backup.$(date +%s.%N)"
 trap "rm -rf $temp" EXIT
 mkdir -p "$temp"
@@ -19,8 +20,17 @@ favorites="$temp/Favoritos"
 cp -r "$APPDATA/Microsoft/Sticky Notes" "$notes"
 cp -r "$USERPROFILE/Favorites" "$favorites"
 
+# Application settings
+tools="/c/programs/ferramentas"
+configs="$temp/Configurações"
+mkdir -p "$configs"
+cp "$tools/defraggler/defraggler.ini" "$configs"
+cp "$tools/ccleaner/ccleaner.ini" "$configs"
+cp "$tools/recuva/recuva.ini" "$configs"
+cp "$tools/speccy/speccy.ini" "$configs"
+
 password=$(cat /dados/documentos/chaves/renatosilva.backup)
-7z a "$temp/$name $(date '+%-d.%-m.%Y %-Hh%M').7z" -p"$password" -xr!desktop.ini -mhe "/dados/documentos" "/dados/programas" "$favorites" "$notes"
+7z a "$temp/$name $(date '+%-d.%-m.%Y %-Hh%M').7z" -p"$password" -xr!desktop.ini -mhe "/dados/documentos" "/dados/programas" "$favorites" "$notes" "$configs"
 rm "$target/$name "*.7z 2> /dev/null || echo "First backup in this device."
 mv "$temp/"*.7z "$target"
 sleep $((3 + delay))
