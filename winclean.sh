971ada022d6af84d63dd07994e9b76ef0cb1c53c
Renato Silva <br.renatosilva@gmail.com>
2012-12-15 05:33:27 -0200

    Run only on shutdown/reboot.

--- winclean.sh
+++ winclean.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Windows Cleanup 2012.12.13
+# Windows Cleanup 2012.12.15
 # Copyright (c) 2012 Renato Silva
 # GNU GPLv2 licensed
 
-# Backup on shutdown
+# Run only on shutdown
 shutdown_happening=$(wevtutil qe system //c:1 //rd:true //f:xml //q:"*[System[(EventID=1074) and TimeCreated[timediff(@SystemTime) <= 60000]]]")
-if [[ -n "$shutdown_happening" ]]; then
-    non_reboot_shutdown=$(echo "$shutdown_happening" | grep -i "<data>desligado</data>")
-    # Wait for phone sync if not rebooting
-    [[ -n "$non_reboot_shutdown" ]] && delay=120
-    mintty -w full bash backup "$delay"
-fi;
+[[ -z "$shutdown_happening" ]] && exit
+
+# Run backup, wait for phone sync if not rebooting
+non_reboot_shutdown=$(echo "$shutdown_happening" | grep -i "<data>desligado</data>")
+[[ -n "$non_reboot_shutdown" ]] && delay=120
+mintty -w full bash backup "$delay"
 
 # Cleanup recent files list from Word Viewer
 filename="$TEMP/winclean.$(date +%s.%N).reg"
