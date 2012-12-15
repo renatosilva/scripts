fecee9a35a543b1115834e7d28d2e48579d38a3c
Renato Silva <br.renatosilva@gmail.com>
2012-12-15 18:24:58 -0200

    New implementation: read a template file instead of command line parameter, and replace non-matched references with empty string.

--- csvt.rb
+++ csvt.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 
-# CSV Transformation 2012.10.28
+# CSV Transformation 2012.12.15
 # Copyright (c) 2009, 2012 Renato Silva
 # GNU GPLv2 licensed
 
-i = File.open(ARGV[0], 'r')  
-o = File.open(ARGV[1], 'w')
+csv = File.open(ARGV[0], 'r')
+out = File.open(ARGV[2], 'rb').read
+row_pattern = /(.*<csv:row>(.*)<\/csv:row>)/
+row_template=out[row_pattern, 1]
 
-i.each_line do |line|
-    nl = ARGV[2].dup
-    r = line.gsub(/\n/, '').split(";")
-    r.each_with_index { |c, n| nl.gsub!(/\$#{n + 1}/, c) }
-    o.puts(nl)
+csv.each_line do |line|    
+    row = out[row_pattern, 2]
+    cols = line.gsub(/\n/, '').split(";")
+    cols.each_with_index { |col, ix| row.gsub!(/\$#{ix + 1}/, col) }
+    row.gsub!(/\$\d+/, '')
+    row << "\n#{row_template}" unless csv.eof?
+    out.sub!(row_template, row)
 end
+
+File.open(ARGV[1], 'wb').write out
