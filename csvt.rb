2b4ff0c904d4242df6985e892f35c1a2bfc76bad
Renato Silva <br.renatosilva@gmail.com>
2012-12-20 21:14:00 -0200

    Allow to specify custom CSV delimiters from command line, or from tag attribute in template.

--- csvt.rb
+++ csvt.rb
@@ -5,18 +5,20 @@
 # GNU GPLv2 licensed
 
 if not ARGV[2]
-    puts "Usage: #{File.basename($0)} <csv file> <output file> <template file>"
+    puts "Usage: #{File.basename($0)} <csv file> <output file> <template file> [delimiters]"
     exit
 end
 
 csv = File.open(ARGV[0], 'r')
 out = File.open(ARGV[2], 'rb').read
-row_pattern = /(.*<csv:row>(.*)<\/csv:row>)/
+row_pattern = /(.*<csv:row(\s*delimiters=['\"]([^'\"]*)['\"])*>(.*)<\/csv:row>)/
 row_template = out[row_pattern, 1]
+delimiters = (ARGV[3] or out[row_pattern, 3])
+delimiters = ";" if delimiters.nil?
 
 csv.each_line do |line|    
-    row = out[row_pattern, 2]
-    cols = line.gsub(/\n/, '').split(";")
+    row = out[row_pattern, 4]
+    cols = line.gsub(/\n/, '').split(/[#{delimiters}]/)
     cols.each_with_index { |col, ix| row.gsub!(/\$#{ix + 1}/, col) }
     row.gsub!(/\$\d+/, '')
     row << "\n#{row_template}" unless csv.eof?
