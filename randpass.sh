20ccce588961bda8bc450a6f95fa8fbeaba9f796
Renato Silva <br.renatosilva@gmail.com>
2012-12-31 05:22:17 -0200

    Remove hard-coded alphabet size.

--- randpass.sh
+++ randpass.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Random Passowrd 2012.12.30
+# Random Passowrd 2012.12.31
 # Copyright (c) 2012 Renato Silva
 # GNU GPLv2 licensed
 
 chars=({{0..9},{a..z},{A..Z}})
+total_chars="${#chars[@]}"
 length="${1:-12}"
 count=1
 
 while [[ "$count" -le "$length" ]]; do
-    echo -n "${chars[RANDOM % 62]}"
+    echo -n "${chars[RANDOM % total_chars]}"
     count=$((count + 1))
 done
 echo
