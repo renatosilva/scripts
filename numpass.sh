5d4d5d4a5cbcb012927355eb492fc6a64f561c75
Renato Silva <br.renatosilva@gmail.com>
2012-12-31 06:46:20 -0200

    Accept upper case letters, and more refactoring.

--- numpass.sh
+++ numpass.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Numeric Password 2012.12.30
+# Numeric Password 2012.12.31
 # Copyright (c) 2012 Renato Silva
 # GNU GPLv2 licensed
 
 # This program converts alphanumeric passwords into number-only passwords.
 
-i=0
-for c in {{a..z},{0..9}}; do
-    i=$((i+1))
-    nums[$i]="$c"
-done
+nums=({{a..z},{0..9},{A..Z}})
 read -p "Password: " -s pwd
 pwd=$(echo "$pwd" | sed -E s/"(.)"/" \\1"/g)
 for c in $pwd; do
-    for ix in {1..36}; do
+    for ix in {0..61}; do
         [[ "${nums[$ix]}" != "$c" ]] && continue
-        [[ "$ix" -gt "26" ]] && ix=$((ix-27))
-        echo -n $((ix%10))
+        [[ "$ix" -gt "25" && "$ix" -lt "36" ]] && ix=$((ix-27))
+        echo -n $(((ix+1)%10))
         break
     done
 done
