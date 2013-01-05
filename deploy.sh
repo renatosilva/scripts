d40edbf985e528bb78296727e13d9ab7cc41458d
Renato Silva <br.renatosilva@gmail.com>
2013-01-05 21:46:58 -0200

    Deploy runcrt and dosconv to root of /local.

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,11 @@ scripts=(
     "check-branches"
     "check-brst-commits"
     "csvt"
+    "dosconv"
     "greprev"
     "numpass"
     "randpass"
+    "runcrt"
     "speak"
     "tz-brazil"
     "winclean"
@@ -18,6 +20,4 @@ for script in "${scripts[@]}"; do
     cp -v "$from"/*"$script"* "/local/$script"
 done
 
-cp -v "$from/runcrt.sh" "/local/runcrt"
-cp -v "$from/dosconv.sh" "/local/dosconv"
 cp -v "$from/msys-aliases.sh" "/etc/profile.d/aliases.sh"
