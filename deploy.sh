b9f73206d1b212cf14378a6486b59a83d8de5fe9
Renato Silva <br.renatosilva@gmail.com>
2013-09-24 18:40:12 -0300

    Create symlinks for runcrt and dosconv on deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ntfs_link() {
+    [[ ! -e "$1" ]] && cmd //c mklink "$1" "$2";
+}
+
 scripts=(
     "backup"
     "check-branches"
@@ -19,3 +23,8 @@ scripts=(
 from=$(dirname "$0")
 for script in "${scripts[@]}"; do cp -v "$from"/*"$script"* "/local/bin/$script"; done
 cp -v "$from/msys-aliases.sh" "/etc/profile.d/aliases.sh"
+
+cd /local/bin
+for link in bzr python ruby; do ntfs_link "$link" runcrt; done
+for link in attrib cmd ipconfig net ping reg schtasks shutdown taskkill; do ntfs_link "$link" dosconv; done
+cd - > /dev/null
