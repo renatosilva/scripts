aee50d44b0a7c247a0375e0a043c48fe7ebb591e
Renato Silva <br.renatosilva@gmail.com>
2013-10-05 19:23:17 -0300

    Notice about beginning of BRT period.

--- check-brst-commits.sh
+++ check-brst-commits.sh
@@ -8,6 +8,20 @@
 # for Bazaar branches containing wrong commit dates with BRST timezone
 # within BRT period, for years 2012 and 2013.
 
+# Please note that this program will not look into the first hour of BRT
+# period, which is when clock is adjusted backward one hour at midnight.
+# For example, if a commit timestamp states 2013-02-16 23:30 -02:00, this
+# script has no means to tell if it was really within BRST period (the "first"
+# 23:30) or rather within BRT period (the "second" 23:30) with a wrong timezone
+# indicator (-02:00 instead of -03:00).
+
+# This is both because the script does not look into parts of a single day,
+# and because even in that case, it is currently unknown if Bazaar is able
+# to tell the exact UTC time, so that such a verification could be performed
+# (in the previous example, first 23:30 would be represented by 01:30 UTC,
+# while second one would show as 02:30 UTC, thus allowing to determinate if
+# the timezone indicator is correct).
+
 check() {
     # Standard time from 2012-02-26 to 2012-10-20
     brt_2012="2012-(02-2[6-9]|0[3-9]-..|10-(0[1-9]|1[0-9]|20))"
