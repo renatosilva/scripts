f5f85f967526b79833d94ba8d3ebcd30bd7f19d6
Renato Silva <br.renatosilva@gmail.com>
2013-10-12 16:20:06 -0300

    Move phone sync to the end of cleanup.

--- winclean.sh
+++ winclean.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# Windows Cleanup 2013.9.25
+# Windows Cleanup 2013.10.12
 # Copyright (c) 2012, 2013 Renato Silva
 # GNU GPLv2 licensed
 
 # Run only on shutdown or --force
 shutdown_happening=$(wevtutil qe system //c:1 //rd:true //f:xml //q:"*[System[(EventID=1074) and TimeCreated[timediff(@SystemTime) <= 60000]]]")
 [[ -z "$shutdown_happening" && "$1" != "--force" ]] && exit
-
-# Run backup on shutdown, wait for phone sync if not rebooting
-non_reboot_shutdown=$(echo "$shutdown_happening" | grep -i "<data>desligado</data>")
-[[ -n "$non_reboot_shutdown" ]] && delay="$1"
-[[ -n "$shutdown_happening" ]] && mintty -w full bash backup --default "$delay" "Esperando pela sincronização do celular"
 
 # Firefox bookmarks cleanup: remove unorganized and descriptions
 database=("$APPDATA/Mozilla/Firefox/profiles/"*"/places.sqlite")
@@ -41,3 +36,8 @@ attrib +h ~/.bash_history
 reg_data=$(reg query 'HKEY_LOCAL_MACHINE\SOFTWARE\Piriform\CCleaner' //ve)
 ccleaner_dir=$(echo "$reg_data" | awk -F'REG_SZ[[:space:]]*' 'NF>1{print $2}')
 "$ccleaner_dir/ccleaner.exe" //auto
+
+# Run backup on shutdown, wait for phone sync if not rebooting
+non_reboot_shutdown=$(echo "$shutdown_happening" | grep -i "<data>desligado</data>")
+[[ -n "$non_reboot_shutdown" ]] && delay="$1"
+[[ -n "$shutdown_happening" ]] && mintty -w full bash backup --default "$delay" "Esperando pela sincronização do celular"
