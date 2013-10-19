2e2ef9c25a7147a503ac9c859bdee48362e14b82
Renato Silva <br.renatosilva@gmail.com>
2013-10-19 13:48:13 -0300

    Make use of parse-options, allowing only missing and status commands, also some code refactoring.

--- check-branches.sh
+++ check-branches.sh
@@ -1,64 +1,44 @@
 #!/bin/bash
 
-# Check branches 2013.10.19
-# Copyright (c) 2012, 2013 Renato Silva
-# GNU GPLv2 licensed
+##
+##     Check Branches 2013.10.19
+##     Copyright (c) 2012, 2013 Renato Silva
+##     GNU GPLv2 licensed
+##
+## This program goes recursively through a directory hierarchy and checks all
+## of the contained Bazaar branches for synchronization status and pending work.
+## In other words, the Bazaar commands missing and status. Usage and options:
+##
+##     @script.name [options]
+##
+##     --root=DIRECTORY   The top directory from where to search for Bazaar
+##                        branches. Current directory is assumed if omitted.
+##     --status-only, -s  Will not perform the missing command, only status one.
+##
 
-# This program goes recursively through a directory hierarchy
-# and run specified commands on every Bazaar branch that is found.
-
-print_branch_name() {
-    printf "%$1s" "--- $(basename "$branch"): " | colordiff 2> /dev/null | sed "s/--- //"
-}
-
-run_bzr_command() {
-    if [[ "$1" = "missing" ]]; then
-        bzr missing --line | grep -v "parent"
-        return
-    fi
-    bzr "$1"
+print_name() {
+    local branch_name="$(basename "$(readlink -f "$1")")"
+    printf "%$2s" "--- $branch_name: " | colordiff 2> /dev/null | sed "s/--- //"
 }
 
 check() {
     branch=$(echo "$0" | sed s/".bzr"//)
-    if [ "$cmds" != "$def_cmds" ]; then
-        first_command="yes"
-        print_branch_name ""
-        for cmd in $cmds; do
-            cd "$branch"
-            run_bzr_command "$cmd"
-            cd - > /dev/null
-            [[ "$first_command" = "no" ]] && continue
-            first_command="no"
-            echo
-        done
+    cd "$branch"
+    if [ -n "$status_only" ]; then
+        print_name "$branch"
+        echo "$(bzr status)"
+        cd - > /dev/null
         return
     fi
-    print_branch_name "-35"
-    for cmd in $(echo "$cmds" | sed -r s/" and ([^,])"/" \\1"/ | sed s/","//g); do
-        cd "$branch"
-        run_bzr_command "$cmd"
-        cd - > /dev/null
-    done
+    print_name "$branch" "-35"
+    bzr missing --line | grep -v "parent"
+    bzr status
+    cd - > /dev/null
 }
 
-root="$1"
-cmds="${@:2}"
-def_cmds="missing and status"
-[ -z "$1" ] && root="."
-[ -z "$cmds" ] && cmds="$def_cmds"
-
-export -f print_branch_name
-export -f run_bzr_command
+source parse-options || exit 1
+export -f print_name
 export -f check
-export def_cmds
-export cmds
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    echo "Usage: $(basename "$0") [root [commmand]...]"
-    echo "Without arguments, root is current directory, and commands are $def_cmds."
-    exit
-fi
-
-find "$root" -name ".bzr" -type d -exec bash -c check '{}' \;
+find "${root:-.}" -name ".bzr" -type d -exec bash -c check '{}' \;
 sleep 3
