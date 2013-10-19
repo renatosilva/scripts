7876cbb3075c4dc34fb2084bda84711c4f676c83
Renato Silva <br.renatosilva@gmail.com>
2013-10-19 12:05:35 -0300

    Better output: remove diff from default commands, summarized missing command, and show colored branch name.

--- check-branches.sh
+++ check-branches.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
 
-# Check branches 2013.10.16
+# Check branches 2013.10.19
 # Copyright (c) 2012, 2013 Renato Silva
 # GNU GPLv2 licensed
 
 # This program goes recursively through a directory hierarchy
 # and run specified commands on every Bazaar branch that is found.
 
+print_branch_name() {
+    printf "%$1s" "--- $(basename "$branch"): " | colordiff 2> /dev/null | sed "s/--- //"
+}
+
+run_bzr_command() {
+    if [[ "$1" = "missing" ]]; then
+        bzr missing --line | grep -v "parent"
+        return
+    fi
+    bzr "$1"
+}
+
 check() {
     branch=$(echo "$0" | sed s/".bzr"//)
     if [ "$cmds" != "$def_cmds" ]; then
-        echo "branch: $branch"
+        first_command="yes"
+        print_branch_name ""
         for cmd in $cmds; do
             cd "$branch"
-            bzr $cmd
+            run_bzr_command "$cmd"
             cd - > /dev/null
+            [[ "$first_command" = "no" ]] && continue
+            first_command="no"
+            echo
         done
         return
     fi
-    for cmd in $(echo "$cmds" | sed -E s/", and ([^,])"/" \\1"/ | sed s/","//g); do
+    print_branch_name "-35"
+    for cmd in $(echo "$cmds" | sed -r s/" and ([^,])"/" \\1"/ | sed s/","//g); do
         cd "$branch"
-        bzr $cmd | grep -v "parent"
+        run_bzr_command "$cmd"
         cd - > /dev/null
     done
 }
 
 root="$1"
 cmds="${@:2}"
-def_cmds="diff, status, and missing"
+def_cmds="missing and status"
 [ -z "$1" ] && root="."
 [ -z "$cmds" ] && cmds="$def_cmds"
+
+export -f print_branch_name
+export -f run_bzr_command
 export -f check
 export def_cmds
 export cmds
@@ -41,5 +61,4 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 fi
 
 find "$root" -name ".bzr" -type d -exec bash -c check '{}' \;
-echo "Finished."
 sleep 3
