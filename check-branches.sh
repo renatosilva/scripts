1fd57121037ce0727bae23018198eccbfd4d6399
Renato Silva <br.renatosilva@gmail.com>
2013-11-01 18:12:38 -0200

    New option --purge-uncommits.

--- check-branches.sh
+++ check-branches.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##
-##     Check Branches 2013.10.23
+##     Check Branches 2013.11.1
 ##     Copyright (c) 2012, 2013 Renato Silva
 ##     GNU GPLv2 licensed
 ##
@@ -11,10 +11,13 @@
 ##
 ##     @script.name [options] [root directory if not current]
 ##
-##         --no-color     Disable colors in output.
-##     -s, --status-only  Will not perform the missing command, only status one.
-##     -h, --help         This help text.
-##
+##         --no-color         Disable colors in output.
+##     -s, --status-only      Do not perform the missing command, only status.
+##     -u, --purge-uncommits  Actually remove from the branch any commit that
+##                            has been reverted with the uncommit command. This
+##                            is done by recreating the branch. Branches with
+##                            pending work are skipped.
+##     -h, --help             This help text.
 ##
 
 print_name() {
@@ -24,16 +27,35 @@ print_name() {
 
 check() {
     branch="$(dirname "$0")"
-    cd "$branch"
+    status=$(bzr status "$branch")
+    if [[ -n "$purge_uncommits" ]]; then
+        if [[ -z "$status" ]]; then
+            branch_old="$branch.$(date +%s.%N).temp"
+            mv "$branch" "$branch_old"
+            branch_output=$(bzr branch "$branch_old" "$branch" 2>&1)
+            config=".bzr/branch/branch.conf"
+            if [[ -f "$branch_old/$config" ]]; then
+                cp "$branch_old/$config" "$branch/$config"
+            else
+                rm -f "$branch/$config"
+            fi
+            rm -rf "$branch_old"
+        else
+            branch_output="Uncommitted changes, ignoring."
+        fi
+    fi
     if [ -n "$status_only" ]; then
-        print_name "$branch"
-        echo "$(bzr status)"
-        cd - > /dev/null
+        [[ -n "$purge_uncommits" ]] && padding="-35"
+        print_name "$branch" "$padding"
+        echo "$branch_output"
+        [[ -n "$status" ]] && echo "$status"
         return
     fi
     print_name "$branch" "-35"
+    [[ -n "$purge_uncommits" ]] && printf "%-35s " "$branch_output"
+    cd "$branch"
     bzr missing --line | grep -v "parent"
-    bzr status
+    [[ -n "$status" ]] && echo "$status"
     cd - > /dev/null
 }
 
@@ -47,5 +69,5 @@ if [[ -t 1 && -z "$no_color" ]]; then
     export green_color="\e[0;32m"
 fi
 
-find "${arguments[0]:-.}" -name ".bzr" -type d -exec bash -c check '{}' \;
+find "${arguments[0]:-.}" -name ".bzr" -type d -print | xargs -l bash -c check | iconv -f cp850 -t iso-8859-1
 sleep 3
