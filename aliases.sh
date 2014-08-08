6c62d2ac663c437268a227cbd984a94d7430294d
Renato Silva <br.renatosilva@gmail.com>
2014-08-08 20:07:51 -0300

    Make generic aliases come first.
    This fixes the colored grep alias not working in MSYS and should prevent similar problems.

--- aliases.sh
+++ aliases.sh
@@ -2,56 +2,6 @@
 # Copyright (c) 2012-2014 Renato Silva
 # GNU GPLv2 licensed
 
-# MSYS
-if [[ $(uname -o) = Msys && $(uname -r) = 1.* ]]; then
-    # Aliases
-    alias whence="type -a"
-    alias grep="grep --color=auto"
-    alias ls="ls --color=auto --show-control-chars"
-    alias msgrep="msgrep --binary-files=text -d skip --color=auto"
-    alias msls="msls -bhAC --more --color=auto --recent --streams"
-    alias update="mingw-get update && mingw-get upgrade 2> /dev/null"
-
-    # SQLite encoding
-    sqlite() {
-        arguments=("$@")
-        database="${arguments[${#arguments[@]}-2]}"
-        sql="${arguments[${#arguments[@]}-1]}"
-
-        [[ -z "${@:2}" ]] && { command sqlite "$@"; return; }
-        [[ ! -f "$sql" ]] && encoding=$(command sqlite "$database" "pragma encoding")
-        [[ -z "$encoding" ]] && { command sqlite "$@"; return; }
-
-        arguments=()
-        for argument in "$@"; do
-            argument=$(iconv -f ISO-8859-1 -t "$encoding" <<< "$argument")
-            arguments+=("$argument")
-        done
-        command sqlite "${arguments[@]}" | iconv -f "$encoding" -t ISO-8859-1
-    }
-fi
-
-# MSYS and MSYS2
-if [[ $(uname -o) = Msys ]]; then
-    # Aliases
-    alias edit="notepad++"
-    alias grep="wgrep"
-    alias find="wfind"
-
-    # Ignore permission errors
-    wgrep() { { command grep "$@" 2>&1 >&3 | command grep -v 'Permission denied'; } 3>&1; }
-    wfind() { { command find "$@" 2>&1 >&3 | command grep -v 'Permission denied'; } 3>&1; }
-
-    # SSH authentication
-    ssh-auth() {
-        [[ -z $(ps | grep ssh-agent) ]] && echo $(ssh-agent) > /tmp/ssh-agent-data.sh
-        [[ -z $SSH_AGENT_PID ]] && source /tmp/ssh-agent-data.sh > /dev/null
-        [[ -z $(ssh-add -l | grep "/home/$(whoami)/.ssh/id_rsa") ]] && ssh-add
-    }
-    ssh() { ssh-auth; command ssh "$@"; }
-    scp() { ssh-auth; command scp "$@"; }
-fi
-
 # Aliases
 alias cat="vimcat"
 alias grepi="grep -i"
@@ -111,3 +61,53 @@ git() {
     esac
     command git "$@"
 }
+
+# MSYS and MSYS2
+if [[ $(uname -o) = Msys ]]; then
+    # Aliases
+    alias edit="notepad++"
+    alias grep="wgrep"
+    alias find="wfind"
+
+    # Ignore permission errors
+    wgrep() { { command grep "$@" 2>&1 >&3 | command grep -v 'Permission denied'; } 3>&1; }
+    wfind() { { command find "$@" 2>&1 >&3 | command grep -v 'Permission denied'; } 3>&1; }
+
+    # SSH authentication
+    ssh-auth() {
+        [[ -z $(ps | grep ssh-agent) ]] && echo $(ssh-agent) > /tmp/ssh-agent-data.sh
+        [[ -z $SSH_AGENT_PID ]] && source /tmp/ssh-agent-data.sh > /dev/null
+        [[ -z $(ssh-add -l | grep "/home/$(whoami)/.ssh/id_rsa") ]] && ssh-add
+    }
+    ssh() { ssh-auth; command ssh "$@"; }
+    scp() { ssh-auth; command scp "$@"; }
+fi
+
+# MSYS
+if [[ $(uname -o) = Msys && $(uname -r) = 1.* ]]; then
+    # Aliases
+    alias whence="type -a"
+    alias grep="grep --color=auto"
+    alias ls="ls --color=auto --show-control-chars"
+    alias msgrep="msgrep --binary-files=text -d skip --color=auto"
+    alias msls="msls -bhAC --more --color=auto --recent --streams"
+    alias update="mingw-get update && mingw-get upgrade 2> /dev/null"
+
+    # SQLite encoding
+    sqlite() {
+        arguments=("$@")
+        database="${arguments[${#arguments[@]}-2]}"
+        sql="${arguments[${#arguments[@]}-1]}"
+
+        [[ -z "${@:2}" ]] && { command sqlite "$@"; return; }
+        [[ ! -f "$sql" ]] && encoding=$(command sqlite "$database" "pragma encoding")
+        [[ -z "$encoding" ]] && { command sqlite "$@"; return; }
+
+        arguments=()
+        for argument in "$@"; do
+            argument=$(iconv -f ISO-8859-1 -t "$encoding" <<< "$argument")
+            arguments+=("$argument")
+        done
+        command sqlite "${arguments[@]}" | iconv -f "$encoding" -t ISO-8859-1
+    }
+fi
