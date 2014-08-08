fa4dbb47929967d2521220e011edfddd91db6945
Renato Silva <br.renatosilva@gmail.com>
2014-08-08 19:31:53 -0300

    Allow iconv to guess the output encoding.

--- dosconv.sh
+++ dosconv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# DOS Encoding Converter 2012.12.10
-# Copyright (c) 2012 Renato Silva
+# DOS Encoding Converter 2014.8.8
+# Copyright (c) 2012, 2014 Renato Silva
 # GNU GPLv2 licensed
 
 # In order to convert output of DOS programs, make symlinks to this script with
 # the name of each program, excluding the exe extension, then make these
 # symlinks available before the original executables in system path.
 
-$(basename "$0").exe "$@" 2>&1 | iconv -f cp850 -t cp1252
+$(basename "$0").exe "$@" 2>&1 | iconv -f cp850
