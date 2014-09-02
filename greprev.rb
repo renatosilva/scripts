d93bfc5e4587f20c87bf475f498789f5b7bedcb1
Renato Silva <br.renatosilva@gmail.com>
2014-09-02 12:36:50 -0300

    Colorize output.

--- greprev.rb
+++ greprev.rb
@@ -17,6 +17,7 @@
 ##         --from=WHERE      Branch location where to search for the pattern
 ##                           (current directory by default).
 ##     -s, --case-sensitive  PATTERN is case-sensitive.
+##         --no-color        Disable colors in output.
 ##
 
 require_relative "easyoptions"
@@ -25,12 +26,30 @@ if not $arguments[0]
     exit
 end
 
+if not $options[:no_color] and ENV["TERM"] =~ /xterm/ and (system("test -t 1") or STDOUT.tty?)
+    $red     = "\e[1;31m"
+    $green   = "\e[0;32m"
+    $cyan    = "\e[0;36m"
+    $yellow  = "\e[0;33m"
+    $normal  = "\e[0m"
+end
+
+def puts(text=nil, color=nil)
+    case color
+        when nil then    super(text)
+        when $cyan then  super(text.gsub(/(#{$arguments[0]})/, "#{$cyan}\\1#{$normal}"))
+        when $red then   super(text.gsub(/(#{$arguments[0]})/, "#{$red}\\1#{$normal}"))
+        else             super("#{color}#{text}#{$normal}")
+    end
+end
+
 pattern = $options[:case_sensitive]?
     /^\s*[+\-].*#{$arguments[0]}.*$/:
     /^\s*[+\-].*#{$arguments[0]}.*$/i
 
 log = $options[:stdin]? STDIN : %x[bzr log --show-diff #{$options[:from]}]
 last_rev, last_file = nil
+first_match = true
 output = []
 
 log.each_line do |line|
@@ -42,15 +61,17 @@ log.each_line do |line|
 
     if line =~ pattern
         if last_rev != nil
-            output << "revision #{last_rev}"
+            puts unless first_match
+            puts "revision #{last_rev}", $green
+            first_match = false
             last_rev = nil
         end
         if last_file != nil
-            output << "\t#{last_file}"
+            puts "\t#{last_file}", $yellow
             last_file = nil
         end
-        output << "\t\t#{line.gsub(/[\n\r]/, '')}"
+        line_color = line.start_with?("+")? $cyan : $red
+        puts "\t\t#{line.gsub(/[\n\r]/, '')}", line_color
     end
 end
-
-puts output
+puts
