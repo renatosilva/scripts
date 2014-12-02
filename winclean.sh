09bce458b00588374ff25379dfa7ad4a43c3fd59
Renato Silva <br.renatosilva@gmail.com>
2014-12-01 23:14:51 -0200

    Fix some problems.
    * Use the SQLite provided by MSYS2.
    * Run regedit as administrator.

--- winclean.sh
+++ winclean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Windows Cleanup 2014.8.8
+# Windows Cleanup 2014.12.1
 # Copyright (c) 2012-2014 Renato Silva
 # GNU GPLv2 licensed
 
@@ -14,8 +14,8 @@ attrib +h ~/.bash_history
 
 # Firefox bookmarks cleanup: remove unorganized and descriptions
 database=("$APPDATA/Mozilla/Firefox/profiles/"*"/places.sqlite")
-sqlite "$database" "delete from moz_bookmarks where parent = (select folder_id from moz_bookmarks_roots where root_name = 'unfiled')"
-sqlite "$database" "delete from moz_items_annos where id in (select i.id from moz_bookmarks b, moz_items_annos i where b.id = i.item_id and b.type = 1
+sqlite3 "$database" "delete from moz_bookmarks where parent = (select folder_id from moz_bookmarks_roots where root_name = 'unfiled')"
+sqlite3 "$database" "delete from moz_items_annos where id in (select i.id from moz_bookmarks b, moz_items_annos i where b.id = i.item_id and b.type = 1
     and title != '' and title not in ('Favoritos do dispositivo móvel', 'Favoritos recentes', 'Mais visitados', 'Tags recentes', 'Histórico', 'Downloads', 'Tags'))"
 
 # Clean up recent files and search history from Notepad++
@@ -30,7 +30,7 @@ trap "rm -r $filename" EXIT
 echo 'Windows Registry Editor Version 5.00
 [HKEY_CURRENT_USER\Software\Microsoft\Office\11.0\Wordview\Data]
 "Settings"=-' > "$filename"
-regedit //s "$filename"
+runas //user:Administrator regedit //s "$filename"
 
 # Clean up WMP junk
 reg_data=$(reg query 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders' //v 'My Music')
