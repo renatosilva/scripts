d23768ce5102034e5d64b7c14f22a0635900ae9a
Renato Silva <br.renatosilva@gmail.com>
2015-03-19 15:15:19 -0300

    Remove --delay option.

--- backup.sh
+++ backup.sh
@@ -21,26 +21,17 @@
 ## Usage:
 ##     @script.name [options]
 ##
-##         --delay=SECONDS       How much time to wait after backup is complete
-##                               and, if --wait-on-shutdown is specified, after
-##                               the lock file has been released. Will produce a
-##                               countdown on command line. Not applied if
-##                               system is currently rebooting.
-##         --delay-message=TEXT  What message to show for the countdown.
 ##         --name=FILENAME       Backup file name, will have date and time
 ##                               appended. Any previous backup with same name
 ##                               will be deleted, right before saving new one.
 ##     -p, --progress            Show backup progress using graphical interface.
-##     -s, --silent              Whether to play a sound when backup is complete
-##                               and after delay time.
+##     -s, --silent              Do not play sound notifications.
 ##         --target=DIR          Custom directory where to store the backup.
-##         --wait-on-shutdown    Wait for a lock file to be released after
+##         --wait-lock=MESSAGE   Wait for FILENAME.lock to be released after
 ##                               backup is complete, if the system is currently
 ##                               shutting down. This is intended for remote
 ##                               synchronization tools having enough time to
-##                               save the resulting file. The lock file is
-##                               created in the target directory as NAME.lock,
-##                               where NAME is the backup name.
+##                               save the resulting file.
 ##     -h, --help                This help text.
 ##
 
@@ -55,10 +46,8 @@ piriform_dir() {
 source easyoptions || exit
 shutdown_happening=$(wevtutil qe system //c:1 //rd:true //f:xml //q:"*[System[(EventID=1074) and TimeCreated[timediff(@SystemTime) <= 60000]]]")
 non_reboot_shutdown=$(echo "$shutdown_happening" | grep -iE "<data>(desligado|desligar o sistema)</data>")
-[[ -n "$shutdown_happening" && -z "$non_reboot_shutdown" ]] && rebooting="yes"
+[[ -n "$shutdown_happening" && -z "$non_reboot_shutdown" ]] && unset wait_lock
 
-[[ -z "$delay" || -n "$rebooting" ]] && delay="0"
-[[ -z "$delay_message" ]] && delay_message="Esperando..."
 [[ -z "$name" ]] && name="Documentos e programas"
 [[ -z "$target" ]] && target="/d/backup"
 [[ -e "$target" ]] || { echo "Target $target not found."; sleep 5; exit 1; }
@@ -123,27 +112,20 @@ tempfile="$temp/$name $(date '+%-d.%-m.%Y %-Hh%M').7z"
 rm "$target/$name "*.7z 2> /dev/null || echo "First backup in this device."
 mv "$temp/"*.7z "$target"
 [[ -z "$silent" ]] && play_sound tada
-[[ -z "$wait_on_shutdown" && "$delay" < 1 ]] && { sleep 3; exit 0; }
 
 # Wait for the lock to be released
-delay_message="$delay_message %${#delay}s segundos"
-if [[ -n "$non_reboot_shutdown" && -n "$wait_on_shutdown" ]]; then
+if [[ -n "$wait_lock" ]]; then
+    wait_message="${wait_lock} %s segundos"
     elapsed="0"
     lock="$target/$name.lock"
     touch "$lock"
     while [[ -e "$lock" ]]; do
         sleep 1
         elapsed=$((elapsed + 1))
-        printf "\r$delay_message " "$elapsed"
+        printf "\r${wait_message} " "$elapsed"
     done
+    printf "\r%${#wait_message}s\r" ""
+    [[ -z "$silent" ]] && play_sound notify
+else
+    sleep 3
 fi
-
-# Wait for specified delay time
-remaining="$delay"
-while [[ "$remaining" > 0 ]]; do
-    printf "\r$delay_message " "$remaining"
-    remaining=$((remaining - 1))
-    sleep 1
-done
-printf "\r%${#delay_message}s\r" ""
-[[ -z "$silent" && "$delay" != "0" ]] && play_sound notify
