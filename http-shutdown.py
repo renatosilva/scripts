bb5d3b448dce94cc252f090fd03b952a41669862
Renato Silva <br.renatosilva@gmail.com>
2015-03-27 19:30:27 -0300

    Migrate from PyWin32 to ctypes.

--- http-shutdown.py
+++ http-shutdown.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# HTTP Shutdown for Windows 2014.8.8
-# Copyright (c) 2014 Renato Silva
+# HTTP Shutdown for Windows 2015.3.27
+# Copyright (c) 2014, 2015 Renato Silva
 # GNU GPLv2 licensed
 
 import sys
 from cgi import parse_qs, escape
 from wsgiref.simple_server import make_server
-from win32api import ExitWindowsEx, GetCurrentProcess
-from win32security import AdjustTokenPrivileges, LookupPrivilegeValue, OpenProcessToken
-from win32security import TOKEN_ADJUST_PRIVILEGES, TOKEN_QUERY
-from win32con import EWX_FORCE, EWX_LOGOFF, EWX_SHUTDOWN, SE_PRIVILEGE_ENABLED, SE_SHUTDOWN_NAME
+from ctypes import windll, wintypes, byref, Structure
+
+SE_SHUTDOWN_NAME = 'SeShutdownPrivilege'
+SE_PRIVILEGE_ENABLED = 0x2
+TOKEN_ADJUST_PRIVILEGES = 0x20
+EWX_SHUTDOWN = 0x1
+EWX_FORCE = 0x4
+
+class LUID(Structure): _fields_ = [('LowPart',  wintypes.DWORD), ('HighPart', wintypes.LONG)]
+class LUID_AND_ATTRIBUTES(Structure): _fields_ = [('Luid', LUID), ('Attributes', wintypes.DWORD)]
+class TOKEN_PRIVILEGES(Structure): _fields_ = [('PrivilegeCount', wintypes.DWORD), ('Privileges', LUID_AND_ATTRIBUTES)]
 
 def logoff_and_shutdown():
-    shutdown_privilege = ((LookupPrivilegeValue(None, SE_SHUTDOWN_NAME), SE_PRIVILEGE_ENABLED),)
-    token_handle = OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY)
-    AdjustTokenPrivileges(token_handle, 0, shutdown_privilege)
-    ExitWindowsEx(EWX_LOGOFF | EWX_SHUTDOWN | EWX_FORCE, 0)
+    token_handle = wintypes.HANDLE()
+    shutdown_privilege = TOKEN_PRIVILEGES(1, LUID_AND_ATTRIBUTES(LUID(), SE_PRIVILEGE_ENABLED))
+    windll.advapi32.LookupPrivilegeValueA(None, SE_SHUTDOWN_NAME, byref(shutdown_privilege.Privileges.Luid))
+    windll.advapi32.OpenProcessToken(windll.kernel32.GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, byref(token_handle))
+    windll.advapi32.AdjustTokenPrivileges(token_handle, False, byref(shutdown_privilege), 0, None, None)
+    windll.user32.ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, 0)
 
 def application(environment, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
