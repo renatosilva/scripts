53c689d60949c9862fd420f34ff129a4d3eb0046
Renato Silva <br.renatosilva@gmail.com>
2015-03-27 21:20:47 -0300

    Some code refactoring.

--- install.sh
+++ install.sh
@@ -5,58 +5,7 @@
 ##     Copyright (C) 2009-2014 Renato Silva
 ##     GNU GPLv2 licensed
 ##
-## This is a set of useful scripts. Some should work on Unix environments in
-## general but some are specific to MSYS2/MSYS. Most of the scripts can be
-## installed and removed with this install script, which should detect system
-## type and install only the ones compatible. Some scripts may require adaption.
-##
-## Main scripts
-##     backup.sh                   Secured file backup with 7-Zip.
-##     bacon-crypt.rb              Base Conversion Encryption algorithm.
-##     bzrcheck.sh                 Pending work check for Bazaar branches.
-##     bzrgrep.rb                  Find out what revision from a Bazaar branch
-##                                 has introduced some specific change.
-##     bzrtags.sh                  Tag synchronization check of Bazaar branches.
-##     conconv-msys.sh             Convert encoding of MSYS console programs.
-##     conconv-msys2.sh            Convert encoding of MSYS2 console programs.
-##     http-shutdown.py            Shut down Windows from a remote HTTP request.
-##     msys2-msys.bat              Allow using MSYS and MSYS2 together.
-##     minget.sh                   Package management helper for MinGW.
-##     ppk-add.sh                  Add a PuTTY private key to current SSH agent.
-##     randpass.sh                 Random password generation.
-##     runcrt.sh                   Avoid timezone problems in MSVCRT programs
-##                                 running in MSYS (for example Python).
-##     testonsave.rb               Run JUnit tests automatically when saving
-##                                 files in Eclipse (works with Kepler).
-##     tz-brazil.rb                Brazilian timezone configuration for MSYS.
-##     winclean.sh                 Windows system cleanup.
-##
-## Other scripts
-##     aliases.sh                   Help with SQLite encoding issues and Bazaar
-##                                  hurried commits, among others.
-##     colordiff.pl                 Colored diff tool by Dave Ewart.
-##     colornote-backup-clean.sh    Keep only last week for backups of the
-##                                  Android ColorNote application.
-##     csvt.rb                      Transform a CSV file according to a given
-##                                  template (for example HTML conversion).
-##     ivona-speak.sh               Read text aloud with IVONA reader.
-##     numpass.sh                   Convert an alphanumeric password into
-##                                  numeric-only.
-##     tabela-price                 How specific advances would affect a loan.
-##                                  (specific to Brazil).
-##     termcolors.sh                Testing terminal colors table.
-##     wifi-reconnections.rb        Generate statistics from a log of Wi-Fi
-##                                  reconnections.
-##
-## Third-party downloads
-##     colormake.sh                 Colorized output for Make and GCC.
-##     easyoptions                  Easy option parsing for Bash and Ruby.
-##     git-bzr.py                   Bridge between Git and Bazaar.
-##     vimcat.sh                    Syntax highlighting for the cat command.
-##     vimpager.sh                  A terminal pager with syntax highlighting.
-##     vpaste.sh                    Command-line pastebin (vpaste.net).
-##
-## Usage: @script.name [options], where options are:
+## Usage: @script.name [options]
 ##
 ##     -r, --remove        Remove the scripts instead of installing them.
 ##     -l, --local         Do not install the third-party downloads.
@@ -74,62 +23,46 @@
 ##                         ROOT is a valid MSYS root.
 ##
 
-eayoptions_url_base="https://github.com/renatosilva/easyoptions/raw/master"
-easyoptions_main="easyoptions:$eayoptions_url_base/easyoptions"
-easyoptions_bash="easyoptions.sh:$eayoptions_url_base/bash/easyoptions.sh"
-easyoptions_ruby="easyoptions.rb:$eayoptions_url_base/ruby/easyoptions.rb"
-vimpager="vimpager:https://github.com/rkitover/vimpager/raw/1b7a351/vimpager"
-vpaste="vpaste:http://vpaste.net/vpaste"
+eayoptions_url_base='https://github.com/renatosilva/easyoptions/raw/master'
+vimpager_url_base='https://github.com/rkitover/vimpager/raw/1b7a351'
+vpaste_url_base='http://vpaste.net/vpaste'
 
-all=(
-    "bacon-crypt"
-    "bzrcheck"
-    "bzrgrep"
-    "bzrtags"
-    "colordiff"
-    "csvt"
-    "dcim-organizer"
-    "dnsdynamic"
-    "numpass"
-    "randpass"
-    "colormake:https://github.com/renatosilva/colormake/raw/master/colormake.sh"
-    "vimcat:https://github.com/rkitover/vimpager/raw/1b7a351/vimcat"
-)
+all=(colormake::'https://github.com/renatosilva/colormake/raw/master/colormake.sh'
+     vimcat::"${vimpager_url_base}/vimcat"
+     bacon-crypt
+     bzrcheck
+     bzrgrep
+     bzrtags
+     colordiff
+     csvt
+     numpass
+     randpass)
 
-windows=(
-    "backup"
-    "colornote-backup-clean"
-    "ivona-speak"
-    "networkmeter-reset"
-    "winclean"
-    "git-bzr:https://github.com/termie/git-bzr-ng/raw/master/git-bzr"
-)
+windows=(git-bzr::'https://github.com/termie/git-bzr-ng/raw/master/git-bzr'
+        backup
+        colornote-backup-clean
+        ivona-speak
+        winclean)
 
-msys1=(
-    "conconv-msys1"
-    "minget"
-    "msys2-msys.bat"
-    "runcrt"
-    "tz-brazil"
-    "$easyoptions_main"
-    "$easyoptions_bash"
-    "$easyoptions_ruby"
-)
+msys1=(easyoptions::"${eayoptions_url_base}/easyoptions"
+       easyoptions.rb::"${eayoptions_url_base}/ruby/easyoptions.rb"
+       easyoptions.sh::"${eayoptions_url_base}/bash/easyoptions.sh"
+       conconv-msys1
+       minget
+       msys2-msys.bat
+       runcrt
+       tz-brazil)
 
-msys2=(
-    "conconv-msys2"
-    "http-shutdown"
-    "$vimpager"
-    "$vpaste"
-)
+msys2=(vimpager::"${vimpager_url_base}/vimpager"
+       vpaste::"${vpaste_url_base}"
+       conconv-msys2
+       http-shutdown)
 
-unix=(
-    "$vimpager"
-    "$vpaste"
-    "$easyoptions_main"
-    "$easyoptions_bash"
-    "$easyoptions_ruby"
-)
+unix=(easyoptions::"${eayoptions_url_base}/easyoptions"
+      easyoptions.rb::"${eayoptions_url_base}/ruby/easyoptions.rb"
+      easyoptions.sh::"${eayoptions_url_base}/bash/easyoptions.sh"
+      vimpager::"${vimpager_url_base}/vimpager"
+      vpaste::"${vpaste_url_base}")
 
 dosconv() {
     read input
@@ -226,7 +159,7 @@ if [[ -z "$remove" ]]; then
     printf "${title_format:-%s}\n" "Installing local scripts"
     for script in $scripts; do
         case "$script" in
-            *:http*)        remote_scripts+=("$script") ;;
+            *::http*)       remote_scripts+=("$script") ;;
             conconv-msys1)  cp -v "$from/conconv-msys1.sh" "$where/conconv.cp850" ;;
             conconv-msys2)  cp -v "$from/conconv-msys2.sh" "$where/conconv.cp850" ;;
             *)              cp -v "$from/$script"* "$where/$script" ;;
@@ -245,13 +178,13 @@ if [[ -z "$remove" ]]; then
     if [[ -z "$local" && -n "${remote_scripts[0]}" ]]; then
         printf "\n${title_format:-%s}\n" "Installing remote scripts"
         for script in "${remote_scripts[@]}"; do
-            download "${script#*:}" "$where" "${script%%:*}"
+            download "${script#*::}" "${where}" "${script%%::*}"
         done
     fi
     winlinks
 else
     winlinks
-    for script in $scripts; do rm -vf "$where/${script%%:*}"; done
+    for script in $scripts; do rm -vf "${where}/${script%%::*}"; done
     [[ $system != msys2 ]] && rm -vf "$to_msys/etc/profile.d/rubylib.sh"
     rm -vf "$to_msys/etc/profile.d/aliases.sh"
     rm -vf "$where/conconv.cp850"
