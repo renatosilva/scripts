1e8ddd5c81f7c8ba4d2ee6e0bbb4664d2c55eb82
Renato Silva <br.renatosilva@gmail.com>
2016-04-30 17:47:55 -0300

    Reimplement the winclean script.

--- winclean.sh
+++ winclean.sh
@@ -1,47 +1,32 @@
 #!/bin/bash
 
-# Windows Cleanup 2015.8.10
-# Copyright (c) 2012-2015 Renato Silva
+# Windows Cleanup 2016.4.30
+# Copyright (c) 2012-2016 Renato Silva
 # GNU GPLv2 licensed
 
-# Kill the running SSH agents
-taskkill //f //im ssh-agent.exe > /dev/null 2>&1
+exec >/dev/null 2>&1
+notepad_plus="${APPDATA}/Notepad++/config.xml"
+bookmarks=("${APPDATA}/Mozilla/Firefox/profiles/"*/places.sqlite)
+ccleaner="$(cat /proc/registry/HKEY_LOCAL_MACHINE/SOFTWARE/Piriform/CCleaner/@)"
+music="$(cat '/proc/registry/HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Explorer/Shell Folders/My Music')"
+firefox() { test -f "${bookmarks}" && sqlite3 "${bookmarks}" "${1}"; }
 
-# Clean up bash history
-if [[ $(uname -or) = 1.*Msys ]]; then
-    rm -f ~/.bash_history
-    touch ~/.bash_history
-    attrib +h ~/.bash_history > /dev/null
-else
-    echo -n > ~/.bash_history
-fi
+# SSH, bash history and WMP
+taskkill -f -im ssh-agent.exe
+truncate --size=0 ~/.bash_history
+test -d "${music}" && find "${music}" -iname '*.jpg' -delete
 
-# Firefox bookmarks cleanup: remove unorganized and descriptions
-database=("$APPDATA/Mozilla/Firefox/profiles/"*"/places.sqlite")
-sqlite3 "$database" "delete from moz_bookmarks where parent = (select folder_id from moz_bookmarks_roots where root_name = 'unfiled')"
-sqlite3 "$database" "delete from moz_items_annos where id in (select i.id from moz_bookmarks b, moz_items_annos i where b.id = i.item_id and b.type = 1
-    and title != '' and title not in ('Favoritos do dispositivo móvel', 'Favoritos recentes', 'Mais visitados', 'Tags recentes', 'Histórico', 'Downloads', 'Tags'))"
+# Unorganized bookmarks and descriptions
+firefox "delete from moz_bookmarks where parent = (select folder_id from moz_bookmarks_roots where root_name = 'unfiled')"
+firefox "delete from moz_items_annos where id in (select i.id from moz_bookmarks b, moz_items_annos i where b.id = i.item_id
+         and b.type = 1 and title != '' and title not in ('Favoritos do dispositivo mÃ³vel', 'Favoritos recentes',
+         'Mais visitados', 'Tags recentes', 'HistÃ³rico', 'Downloads', 'Tags'))"
 
-# Clean up recent files and search history from Notepad++
-npp_config="$APPDATA/Notepad++/config.xml"
-sed -i -E "/^\\s+<File\\s+filename=.*$/d" "$npp_config"
-sed -i -E "/^\\s+<(Find|Replace)\\s+name=.*$/d" "$npp_config"
-unix2dos --quiet "$npp_config" 2> /dev/null
+# Notepad++ recent files and searches
+sed -i -r '/^\s+<(Find|Replace)\s+name=.*$/d' "${notepad_plus}"
+sed -i -r '/^\s+<File\s+filename=.*$/d' "${notepad_plus}"
+unix2dos "${notepad_plus}"
 
-# Clean up recent files list from Word Viewer
-reg delete 'HKEY_CURRENT_USER\Software\Microsoft\Office\11.0\Wordview\Data' //v Settings //f > /dev/null
-
-# Clean up WMP junk
-reg_data=$(reg query 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders' //v 'My Music')
-music=$(echo "$reg_data" | awk -F'REG_SZ[[:space:]]*' 'NF>1{print $2}')
-[[ -d "$music" ]] && find "$music" -iname "*.jpg" -delete
-
-# Let CCleaner do its job
-reg_data=$(reg query 'HKEY_LOCAL_MACHINE\SOFTWARE\Piriform\CCleaner' //ve)
-ccleaner_dir=$(echo "$reg_data" | awk -F'REG_SZ[[:space:]]*' 'NF>1{print $2}')
-test -z "$ccleaner_dir" && ccleaner_dir='/c/Program Files/CCleaner'
-"$ccleaner_dir/ccleaner.exe" //auto
-[[ "$1" != --wait ]] && exit
-while [[ -n $(tasklist | grep -i ccleaner) ]]; do
-    sleep 1
-done
+# Execute CCleaner
+PATH="${PATH}:$(cygpath --unix "${ccleaner}")" ccleaner //auto
+[[ "${1}" = --wait ]] && while test -n "$(tasklist | grep -i ccleaner)"; do sleep 1; done
